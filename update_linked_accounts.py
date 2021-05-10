b31131a8ead9b0b934a7fdd31e4cebbff64cdd77
Chris Delis <cedelis@uillinois.edu>
2021-05-10 16:40:39 -0500

    Remove annoying BOM character if it exists

--- update_linked_accounts.py
+++ update_linked_accounts.py
@@ -4,6 +4,7 @@ import json
 import os
 import requests
 import sys
+import codecs
 from requests.exceptions import HTTPError
 
 from settings import *
@@ -66,7 +67,29 @@ def alma_put(resource, apikey, payload=None, params=None, fmt='json'):
         raise err
     return r
 
+
+def remove_bom_inplace(path):
+    """Removes BOM mark, if it exists, from a file and rewrites it in-place"""
+    buffer_size = 4096
+    bom_length = len(codecs.BOM_UTF8)
+ 
+    with open(path, "r+b") as fp:
+        chunk = fp.read(buffer_size)
+        if chunk.startswith(codecs.BOM_UTF8):
+            i = 0
+            chunk = chunk[bom_length:]
+            while chunk:
+                fp.seek(i)
+                fp.write(chunk)
+                i += len(chunk)
+                fp.seek(bom_length, os.SEEK_CUR)
+                chunk = fp.read(buffer_size)
+            fp.seek(-bom_length, os.SEEK_CUR)
+            fp.truncate()
+
 def read_report_generator(report):
+    remove_bom_inplace(report)
+
     with open(report, encoding='utf-8-sig') as fh:
         # is it tab-delimeted?
         line = fh.readline(1024)
