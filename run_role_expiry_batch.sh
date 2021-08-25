b922a2c7d6359106ff522279dcfe88a269b8841d
Chris Delis <cedelis@uillinois.edu>
2021-08-25 16:57:02 -0500

    remove commented-out lines

--- run_role_expiry_batch.sh
+++ run_role_expiry_batch.sh
@@ -1,6 +1,6 @@
 RUN=20210825
 
 nohup sh role_expiry_batch1.sh $RUN &
-#nohup sh role_expiry_batch2.sh $RUN &
-#nohup sh role_expiry_batch3.sh $RUN &
-#nohup sh role_expiry_batch4.sh $RUN &
+nohup sh role_expiry_batch2.sh $RUN &
+nohup sh role_expiry_batch3.sh $RUN &
+nohup sh role_expiry_batch4.sh $RUN &
