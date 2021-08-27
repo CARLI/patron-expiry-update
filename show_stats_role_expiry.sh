9261ec1f32c157cc55e52a92993286bdabe537a4
Chris Delis <cedelis@uillinois.edu>
2021-08-27 10:45:29 -0500

    bug fix

--- show_stats_role_expiry.sh
+++ show_stats_role_expiry.sh
@@ -2,7 +2,6 @@ RUN=20210825
 cd linked_user_with_role_expiration_date_${RUN}_output 
 for i in $(ls); do
   echo $i
-  echo $i | cut -d- -f5 | cut -d. -f1
   egrep "TOTAL|SUCCESS|success:|updates *required|errors:" $i
   echo
 done
