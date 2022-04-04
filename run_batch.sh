09241c5afe0d861e0d1e91d0219a73b664f7d541
Chris Delis <cedelis@uillinois.edu>
2022-04-04 17:00:37 -0500

    Getting ready to run as cron job

--- run_batch.sh
+++ run_batch.sh
@@ -1,6 +1,39 @@
-RUN=20210302
+#!/bin/sh
+cd `dirname $0`
 
-nohup sh batch1.sh $RUN &
-nohup sh batch2.sh $RUN &
-nohup sh batch3.sh $RUN &
-nohup sh batch4.sh $RUN &
+today=`date +"%Y%m%d"`
+flag="/home/carli/scripts/patron-expiry-update/$today/FINISHED"
+
+max=120
+cnt=0
+while true; do
+  echo "cnt: $cnt"
+  if [ -f "${flag}" ]; then
+    echo "flag file exists: $flag"
+    echo "executing..."
+    break
+  else
+    echo "flag file doesn't exist: $flag"
+    echo "sleeping 60 seconds..."
+    sleep 60
+    cnt=$(expr $cnt + 1)
+    if [ $cnt -gt $max ]; then
+      echo "timed out waiting for flag file: $flag"
+      exit 1
+    fi
+  fi
+done
+
+date
+
+echo "executing batch 1..."
+./batch1.sh $today &
+echo "executing batch 2..."
+./batch2.sh $today &
+echo "executing batch 3..."
+./batch3.sh $today &
+echo "executing batch 4..."
+./batch4.sh $today &
+wait
+
+date
